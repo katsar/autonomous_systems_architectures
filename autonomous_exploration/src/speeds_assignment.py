894e6ba28e2e56c748c6a55eae7000c84cfa5d96
katsar <john.katsaronas@gmail.com>
2015-12-24 14:07:25 +0200

    Obstacle avoidance using the laser sensor

--- autonomous_exploration/src/speeds_assignment.py
+++ autonomous_exploration/src/speeds_assignment.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import numpy as np
+import random
+import math
 import rospy
 
 from sensor_msgs.msg import Range
@@ -83,6 +86,34 @@ class RobotController:
 
       # YOUR CODE HERE ------------------------------------------------------
       # Adjust the linear and angular velocities using the five sonars values
+      '''e = 0.0001 * random.randint(1,10)
+
+      if front >= 280:
+	front  = 10e16
+      if left >= 280:
+	left = 10e16
+      if right >= 280:
+	right  = 10e16
+      if r_left >= 280:
+	r_left = 10e16
+      if r_right >= 280:
+	r_right  = 10e16
+  
+
+      force_front = 1/(front + e)**2
+      force_left = 1/(left + e)**2
+      force_right = 1/(right + e)**2
+      force_r_left = 1/(r_left + e)**2
+      force_r_right = 1/(r_right + e)**2
+
+
+      linear = 0.7 * front/(r_left+r_right + front + e)
+      angular = ((0.3 * left + 0.2 * left/(r_left+e) * random.randint(1,10) * 0.1) - (0.3 * right + 0.2 * right/(r_right+e) * random.randint(1,10) * 0.1)) * force_front
+      if linear <= 0.10 and angular <= 0.01 :
+	if angular >= 0:
+	     angular = - 0.01 * random.randint(10,20) 
+	else:
+	     angular = + 0.01 * random.randint(10,20)'''
 
       # ---------------------------------------------------------------------
 
@@ -99,6 +130,22 @@ class RobotController:
 
       # YOUR CODE HERE ------------------------------------------------------
       # Adjust the linear and angular velocities using the laser scan
+      scan = np.clip(scan,0,10)
+      degrees = np.matrix(scan) * 667/240 
+      deg = np.cos(np.array(np.arange(-166,167,1))/166)
+      f = np.array(np.arange(-166,167,1))/166
+      f2 = np.array(np.arange(-150,150,1))/150
+      gaussianLinear = np.exp(-f**2/(2*(0.5 ** 2)))
+      linear = 0.6 * (np.mean(scan[166:499] * gaussianLinear))/10
+
+      gaussianAngular = np.exp(-f2**2/(2*(0.5 ** 2)))
+      #deg2 = np.cos(np.array(np.arange(-100,100,1))/100)
+      left = (np.mean(scan[0:300] * gaussianAngular))/10
+
+      #deg3 = np.cos(np.array(np.arange(-100,100,1))/100)
+      right = np.mean(scan[367:667] * gaussianAngular)/10
+	
+      angular = (1/linear) * (-left + right)/1.5
 
       # ---------------------------------------------------------------------
 
@@ -121,6 +168,21 @@ class RobotController:
 
       # Combine the speeds following the subsumption architecture
       # YOUR CODE HERE ------------------------------------------------------
+      '''front_sonar = self.sonar_aggregation.sonar_front_range
+      left_sonar = self.sonar_aggregation.sonar_left_range
+      right_sonar = self.sonar_aggregation.sonar_right_range
+      
+      # linear velocity
+      if front_sonar >= 0.5 or front_sonar >= 2.5:
+	self.linear_velocity = l_laser
+      else :
+	self.linear_velocity = l_sonar
+
+      # angular velocity
+      if (left_sonar >= 0.5 or left_sonar >= 2.5) or (right_sonar >= 0.5 or right_sonar >= 2.5):
+	self.angular_velocity = a_laser
+      else :
+	self.angular_velocity = a_sonar'''
 
       # ---------------------------------------------------------------------
 
@@ -136,12 +198,13 @@ class RobotController:
       [l_laser, a_laser] = self.produceSpeedsLaser()
       [l_goal, a_goal] = self.navigation.velocitiesToNextSubtarget()
 
-      self.linear_velocity  = 0
-      self.angular_velocity = 0
+      self.linear_velocity  = l_laser
+      self.angular_velocity = a_laser
         
       # Get the speeds using the motor schema approach
       # YOUR CODE HERE ------------------------------------------------------
-        
+      '''self.linear_velocity = (l_sonar + l_laser)/2
+      self.angular_velocity = (a_sonar + a_laser)/2'''
       # ---------------------------------------------------------------------
 
     # Assistive functions - Do you have to call them somewhere?
